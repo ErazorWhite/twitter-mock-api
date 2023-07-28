001ca80f634ce7c6a9dcf03e034792a431f8c41b
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-07-28 21:34:12 +0300

    added server files and db.json

---
+++ db.js
@@ -0,0 +1,65 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.usersWithPosts = exports.createRandomUsersWithPosts = exports.createRandomPost = exports.createRandomUser = void 0;
+var faker_1 = require("@faker-js/faker");
+var fs = require("fs");
+// Setting our own seed to get consistent faker js results
+faker_1.faker.seed(2007);
+// creates a date past before
+faker_1.faker.date.past({ refDate: '2023-07-26T00:00:00.000Z' });
+function createRandomUser() {
+    return {
+        userId: faker_1.faker.string.uuid(),
+        username: faker_1.faker.internet.userName(),
+        email: faker_1.faker.internet.email(),
+        avatar: faker_1.faker.image.avatar(),
+        following: faker_1.faker.number.int({ min: 0, max: 500 }),
+        followers: faker_1.faker.number.int({ min: 0, max: 50000 }),
+        country: faker_1.faker.location.country(),
+        city: faker_1.faker.location.city(),
+        url: faker_1.faker.internet.url(),
+        description: faker_1.faker.lorem.text(),
+        banner: faker_1.faker.image.urlLoremFlickr({ category: 'abstract' }),
+        registeredAt: faker_1.faker.date.past(),
+    };
+}
+exports.createRandomUser = createRandomUser;
+function createRandomPost(users) {
+    var randomUserIndex = faker_1.faker.number.int({ min: 0, max: users.length - 1 });
+    var author = users[randomUserIndex];
+    return {
+        createdAt: faker_1.faker.date.past(),
+        author: author.username,
+        avatar: author.avatar,
+        message: faker_1.faker.lorem.paragraph(),
+        image: faker_1.faker.image.urlLoremFlickr({ category: 'cat' }),
+        authorId: author.userId,
+        messageId: faker_1.faker.string.uuid(),
+    };
+}
+exports.createRandomPost = createRandomPost;
+function createRandomUsersWithPosts() {
+    var users = [];
+    for (var i = 0; i < 10; i++) {
+        var user = createRandomUser();
+        users.push(user);
+    }
+    var posts = [];
+    users.forEach(function () {
+        var numPosts = faker_1.faker.number.int({ min: 1, max: 5 }); // You can adjust the number of posts per user here
+        for (var i = 0; i < numPosts; i++) {
+            var post = createRandomPost(users);
+            posts.push(post);
+        }
+    });
+    return { users: users, posts: posts };
+}
+exports.createRandomUsersWithPosts = createRandomUsersWithPosts;
+exports.usersWithPosts = createRandomUsersWithPosts();
+// Generate data and write it to db.json
+function generateData() {
+    var data = createRandomUsersWithPosts();
+    fs.writeFileSync('db.json', JSON.stringify(data, null, 2));
+}
+// Call the function to generate and save data to db.json
+generateData();
---
+++ db.ts
@@ -0,0 +1,102 @@
+import { faker } from '@faker-js/faker';
+import * as fs from 'fs';
+
+// Setting our own seed to get consistent faker js results
+faker.seed(2007);
+// creates a date past before
+faker.date.past({ refDate: '2023-07-26T00:00:00.000Z' });
+
+interface User {
+  userId: string;
+  username: string;
+  email: string;
+  avatar: string;
+  following: number;
+  followers: number;
+  country: string;
+  city: string;
+  url: string;
+  banner: string;
+  description: string;
+  registeredAt: Date;
+}
+
+export function createRandomUser(): User {
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    avatar: faker.image.avatar(),
+    following: faker.number.int({ min: 0, max: 500 }),
+    followers: faker.number.int({ min: 0, max: 50000 }),
+    country: faker.location.country(),
+    city: faker.location.city(),
+    url: faker.internet.url(),
+    description: faker.lorem.text(),
+    banner: faker.image.urlLoremFlickr({ category: 'abstract' }),
+    registeredAt: faker.date.past(),
+  };
+}
+
+interface Post {
+  createdAt: Date;
+  author: string;
+  avatar: string;
+  message: string;
+  image: string;
+  authorId: string;
+  messageId: string;
+}
+
+export function createRandomPost(users: User[]): Post {
+  const randomUserIndex = faker.number.int({ min: 0, max: users.length - 1 });
+  const author = users[randomUserIndex];
+
+  return {
+    createdAt: faker.date.past(),
+    author: author.username,
+    avatar: author.avatar,
+    message: faker.lorem.paragraph(),
+    image: faker.image.urlLoremFlickr({ category: 'cat' }),
+    authorId: author.userId,
+    messageId: faker.string.uuid(),
+  };
+}
+
+interface IData {
+  users: User[];
+  posts: Post[];
+}
+
+export function createRandomUsersWithPosts(): IData {
+  const users: User[] = [];
+
+  for (let i = 0; i < 10; i++) {
+    const user = createRandomUser();
+    users.push(user);
+  }
+
+  const posts: Post[] = [];
+
+  users.forEach(() => {
+    const numPosts = faker.number.int({ min: 1, max: 5 }); // You can adjust the number of posts per user here
+
+    for (let i = 0; i < numPosts; i++) {
+      const post = createRandomPost(users);
+      posts.push(post);
+    }
+  });
+
+  return { users, posts };
+}
+
+export const usersWithPosts: IData = createRandomUsersWithPosts();
+
+// Generate data and write it to db.json
+function generateData() {
+  const data = createRandomUsersWithPosts();
+  fs.writeFileSync('db.json', JSON.stringify(data, null, 2));
+}
+
+// Call the function to generate and save data to db.json
+generateData();
